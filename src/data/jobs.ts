b82cb5f088311d0ce0f84f6b65497fd013d16389
Jackson <jackson1712@hotmail.com>
2024-02-26 18:13:02 -0300

    feat: add new endpoint

---
+++ src/data/jobs.ts
@@ -0,0 +1,31 @@
+import { api } from '@/app/api/axios'
+import { getSession } from 'next-auth/react'
+import { redirect } from 'next/navigation'
+
+interface GetJobsPropsResponse {
+  jobs: {
+    id: string
+    companyName: string
+    title: string
+    description: string
+    created_at: Date
+  }[]
+}
+
+export async function getJobs(page: number): Promise<GetJobsPropsResponse> {
+  const session = await getSession()
+
+  if (!session) {
+    redirect('/signIn')
+  }
+
+  const token = session.user.access_token
+
+  const response = await api.get(`/jobs/fetch/list?page=${page}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
+
+  return response.data
+}
