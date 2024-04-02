3b8b2c6b444fe9b8518c431754d8249bc725db86
Jackson <jackson1712@hotmail.com>
2024-04-02 14:58:54 -0300

    refactor: change bg textarea

--- src/app/components/Textarea.tsx
+++ src/app/components/Textarea.tsx
@@ -7,7 +7,7 @@ const Textarea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
     return (
       <textarea
         ref={ref}
-        className="min-h-28 w-full resize-none rounded-lg border border-zinc-300 px-3 py-2 shadow-sm outline-none focus:border-violet-400 focus:ring-4 focus:ring-violet-100 dark:border-zinc-700 dark:bg-zinc-800 dark:text-zinc-100 dark:placeholder-zinc-400 dark:focus:border-violet-500 dark:focus:ring-violet-500/20"
+        className="min-h-28 w-full resize-none rounded-lg border border-zinc-300 bg-white/75 px-3 py-2 shadow-sm outline-none focus:border-violet-400 focus:ring-4 focus:ring-violet-100"
         {...props}
       />
     )
