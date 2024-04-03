1094314976b5e6b276c650787ab2a1fa02fe0c4c
Jackson <jackson1712@hotmail.com>
2024-04-03 16:57:09 -0300

    fix

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,65 +1,6 @@
-import NextAuth from 'next-auth/next'
-import { NextAuthOptions } from 'next-auth'
-import CredentialsProvider from 'next-auth/providers/credentials'
-import { api } from '@/app/api/axios'
-
-const nextAuthOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: 'credentials',
-      credentials: {
-        email: {
-          label: 'email',
-          type: 'email',
-          placeholder: 'exemplo@email.com',
-        },
-        password: {
-          label: 'password',
-          type: 'password',
-          placeholder: 'Mínimo 6 caracteres',
-        },
-      },
-      async authorize(credentials) {
-        const data = {
-          email: credentials?.email,
-          password: credentials?.password,
-        }
-
-        const response = await api.post('/sessions', data, {
-          headers: { 'Content-Type': 'application/json' },
-        })
-        const user = {
-          ...response.data.user,
-          access_token: response.data.access_token,
-        }
-
-        if (user) {
-          return user
-        }
-
-        return null
-      },
-    }),
-  ],
-  pages: {
-    signIn: '/signIn',
-  },
-  session: {
-    strategy: 'jwt',
-    maxAge: 60 * 60 * 24 * 2, // 2 days
-  },
-  callbacks: {
-    async jwt({ token, user }) {
-      user && (token.user = user)
-      return token
-    },
-    async session({ session, token }) {
-      session.user = token.user as any
-      return session
-    },
-  },
-}
+import { nextAuthOptions } from '@/lib/auth'
+import NextAuth from 'next-auth/'
 
 const handler = NextAuth(nextAuthOptions)
 
-export { handler as GET, handler as POST, nextAuthOptions }
+export { handler as GET, handler as POST }
