524d790dfbd96b7324fcce99c0c147e932d63418
Jackson <jackson1712@hotmail.com>
2024-04-03 12:55:56 -0300

    fix

--- src/app/(auth-routes)/layout.tsx
+++ src/app/(auth-routes)/layout.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from 'next-auth'
-import { nextAuthOptions } from '../api/auth/[...nextauth]/route'
+import { nextAuthOptions } from '../api/auth/[...nextauth/]/route'
 import { redirect } from 'next/navigation'
 import { SidebarDeveloper } from '@/app/components/SidebarDeveloper'
 import { SidebarCompany } from '../components/SidebarCompany'
--- src/app/api/auth/[...nextauth/]/route.ts
+++ src/app/api/auth/[...nextauth/]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth, { NextAuthOptions } from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
-import { api } from '../../axios'
+import { api } from '../../../axios'
 
 const nextAuthOptions: NextAuthOptions = {
   providers: [
